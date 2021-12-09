2858a92d82284e37bb1aa329d42f1db7a0abbc65
Aitzhan <aitzhan@mail.ru>
2021-12-09 22:12:58 +0600

    refactor: create home page instead of leaving the gallery in App

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,31 +1,8 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-
-import { Container, Row, Col } from "react-bootstrap";
-import SellerCard from "../SellerCard/SellerCard";
-import CategoriesList from "../CategoriesList/CategoriesList";
-
-import { CATEGORIES } from "../../consts/consts";
-import { selectSellersIds } from "../../store/sellersSlice";
+import React from "react";
+import HomePage from "../pages/HomePage";
 
 function App() {
-  const [filter, setFilter] = useState(CATEGORIES.ALL);
-  const sellersIds = useSelector((state) => selectSellersIds(state));
-
-  const handleCategoryClick = (category) => {
-    setFilter(category);
-  };
-
-  return (
-    <Container fluid>
-      <Row>
-        <CategoriesList handleCategoryClick={handleCategoryClick} />
-        {sellersIds.map((id) => (
-          <SellerCard id={id} key={id} filter={filter} />
-        ))}
-      </Row>
-    </Container>
-  );
+  return <HomePage />;
 }
 
 export default App;
