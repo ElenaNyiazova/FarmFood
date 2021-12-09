379d0dbfc9eb728380269d2efdc8c836243fae37
Elena Niiazova <Elena_Niiazova@epam.com>
2021-12-09 21:11:23 +0300

    beck-first-files
    added Endpoints

--- src/main/java/epam/FarmFood/constants/Endpoints.java
+++ src/main/java/epam/FarmFood/constants/Endpoints.java
@@ -1,4 +1,20 @@
 package epam.FarmFood.constants;
 
+@SuppressWarnings("squid:S1214")
 public interface Endpoints {
+
+    String CSS = "/static/css/{.*\\.css$}";
+
+    String JS = "/static/js/{.*\\.js$}";
+
+    String MANIFEST = "/manifest.json";
+
+    String ICON = "/favicon.ico";
+
+    String ID = "/{id}";
+
+    String PAGE_USERS = "/users";
+
+    String PAGE_ERROR = "/error";
+
 }
