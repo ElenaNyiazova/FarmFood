4547611004315dc33a27ad286579835743ad696d
Elena Niiazova <Elena_Niiazova@epam.com>
2021-12-10 18:27:03 +0300

    back-Maven-and-must-it-work
    Added Maven, and some classes

--- src/main/java/com/epam/FarmFood/constants/Endpoints.java
+++ src/main/java/com/epam/FarmFood/constants/Endpoints.java
@@ -17,4 +17,39 @@ public interface Endpoints {
 
     String PAGE_ERROR = "/error";
 
+    String MAIN_PAGE = "/main";
+
+    /**
+     * API calls:
+     */
+
+    String API_PREFIX = "/api/v1";
+
+    /**
+     * User management
+     */
+
+    String PAGE_LOGIN = "/login";
+
+    String PAGE_USER_PROFILE = "/user";
+
+    String USER_ID = "/{id}";
+
+    String PAGE_USER_REGISTRATION = PAGE_USER_PROFILE + "/registration";
+
+    /**
+     * Admin management
+     */
+
+    String ADMIN = "/admin";
+
+    /**
+     * Actions
+     */
+
+    String SUCCESS = "/success ";//сообщение об удачном действии: регистрации, сохранении и т.д
+
+    String WARNING = "/warning";//сообщение об ошибке
+
+
 }
--- src/main/java/com/epam/FarmFood/enums/Role.java
+++ src/main/java/com/epam/FarmFood/enums/Role.java
@@ -2,8 +2,8 @@ package com.epam.FarmFood.enums;
 
 public enum Role {
     NO_AUTOR_USER("no_autor_user"),
-    USER_ROLE("user_role"),
-    SELLER("seller"),
+    BUYER_ROLE("buyer_role"),
+    SELLER_ROLE("seller_role"),
     ADMIN_ROLE("admin_role");
 
     private String name;
