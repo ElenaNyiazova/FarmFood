a8844d7d528369c4e2bb4a4a7c6d3c9a6d8d0f67
Aitzhan <aitzhan@mail.ru>
2021-12-11 13:41:38 +0600

    feat: add id from useParams to SellerPage

--- frontend/src/components/pages/SellerPage.jsx
+++ frontend/src/components/pages/SellerPage.jsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import { useSelector } from "react-redux";
+import { selectSellerById } from "../../store/sellersSlice";
 
 export const SellerPage = () => {
+  let { id } = useParams();
+  console.log(id);
+  const sellerInfo = useSelector((state) => selectSellerById(state, id));
+  console.log(sellerInfo);
   return <div>Seller</div>;
 };
