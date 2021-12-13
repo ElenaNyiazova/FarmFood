48b3e8d9bb95f1babded99236297e43daf306c77
vito-a <vito_a@meta.ua>
2021-12-13 11:49:49 +0200

    FF-008 - https://github.com/ElenaNyiazova/FarmFood/issues/8 - basic database structure adding

--- sql/farmfood.sql
+++ sql/farmfood.sql
@@ -2,9 +2,50 @@
 -- CREATE DATABASE farmfood WITH OWNER = postgres ENCODING = 'UTF8' TABLESPACE = pg_default CONNECTION LIMIT = -1;
 --/c farmfood;
 
-CREATE TABLE users (
+-- DROP TABLE IF EXISTS categories;
+CREATE TABLE IF NOT EXISTS categories (
   id SERIAL PRIMARY KEY,
-  first_name TEXT,
-  last_name TEXT,
+  name TEXT,
+  description TEXT
+);
+
+-- DROP TABLE IF EXISTS products;
+CREATE TABLE IF NOT EXISTS products (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  description TEXT
+);
+
+-- DROP TABLE IF EXISTS cities;
+CREATE TABLE IF NOT EXISTS cities (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  description TEXT
+);
+
+-- DROP TABLE IF EXISTS sellers;
+CREATE TABLE IF NOT EXISTS sellers (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  description TEXT
+);
+
+-- DROP TABLE IF EXISTS seller_categories;
+CREATE TABLE IF NOT EXISTS seller_categories (
+  id SERIAL PRIMARY KEY,
+  category_id BIGINT NOT NULL
+);
+
+-- DROP TABLE IF EXISTS seller_products;
+CREATE TABLE IF NOT EXISTS seller_products (
+  id SERIAL PRIMARY KEY,
+  product_id BIGINT NOT NULL
+);
+
+-- DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  firstname TEXT,
+  lastname TEXT,
   email TEXT UNIQUE NOT NULL
 );
