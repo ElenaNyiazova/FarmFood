4a5c6c0effd939fd2be41b55ea8ed4c1488f83f4
Elena Niiazova <Elena_Niiazova@epam.com>
2021-12-14 14:22:38 +0300

    back-Maven-and-must-it-work
    SERVER is running!!!!

--- src/main/java/com/gpec/FarmFood/servises/UserService.java
+++ src/main/java/com/gpec/FarmFood/servises/UserService.java
@@ -16,7 +16,7 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username);
+        User user = userRepository.findByName(username);
 
         if (user == null) {
             throw new UsernameNotFoundException("User not found");
