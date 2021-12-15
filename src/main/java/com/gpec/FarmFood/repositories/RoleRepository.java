e3a08d96fa2752014141c16b143f837a4e8455d1
Elena Niiazova <Elena_Niiazova@epam.com>
2021-12-15 16:38:57 +0300

    back-Maven-and-must-it-work
    changes

---
+++ src/main/java/com/gpec/FarmFood/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.gpec.FarmFood.repositories;
+
+import com.gpec.FarmFood.model.db.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
