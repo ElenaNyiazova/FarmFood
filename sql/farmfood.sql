66e67ddd6f3dc961eeded9f63aa315a3c8890ae4
vito-a <vito_a@meta.ua>
2021-12-16 03:06:28 +0200

    FF-010 - https://github.com/ElenaNyiazova/FarmFood/issues/10 - Add images to db - images handling adding

--- sql/farmfood.sql
+++ sql/farmfood.sql
@@ -24,25 +24,25 @@
 -- -------------------------------------------------------
 -- Table `farmfood`.`categories`
 -- -------------------------------------------------------
---CREATE TABLE IF NOT EXISTS categories (
---  id SERIAL PRIMARY KEY,
---  name TEXT,
---  description TEXT,
---  parent_id BIGINT
---);
+CREATE TABLE IF NOT EXISTS categories (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  description TEXT,
+  parent_id BIGINT
+);
 
 -- -------------------------------------------------------
 -- Table `farmfood`.`products`
 -- -------------------------------------------------------
---CREATE TABLE IF NOT EXISTS products (
---  id SERIAL PRIMARY KEY,
---  name TEXT,
---  description TEXT,
---  weight REAL,
---  price REAL,
---  image TEXT,
---  image_blob BYTEA
---);
+CREATE TABLE IF NOT EXISTS products (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  description TEXT,
+  weight REAL,
+  price REAL,
+  image TEXT,
+  image_blob BYTEA
+);
 
 -- -------------------------------------------------------
 -- Table `farmfood`.`product_categories`
@@ -58,37 +58,37 @@ CREATE TABLE IF NOT EXISTS product_categories (
 -- -------------------------------------------------------
 -- Table `farmfood`.`cities`
 -- -------------------------------------------------------
---CREATE TABLE IF NOT EXISTS cities (
---  id SERIAL PRIMARY KEY,
---  name TEXT,
---  description TEXT
---);
+CREATE TABLE IF NOT EXISTS cities (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  description TEXT
+);
 
 -- -------------------------------------------------------
 -- Table `farmfood`.`contacts`
 -- -------------------------------------------------------
---CREATE TABLE IF NOT EXISTS contacts (
---  id SERIAL PRIMARY KEY,
---  name TEXT,
---  phone TEXT,
---  email TEXT,
---  site TEXT,
---  instagram TEXT,
---  viber TEXT,
---  whatsapp TEXT,
---  telegram TEXT
---);
+CREATE TABLE IF NOT EXISTS contacts (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  phone TEXT,
+  email TEXT,
+  site TEXT,
+  instagram TEXT,
+  viber TEXT,
+  whatsapp TEXT,
+  telegram TEXT
+);
 
 -- -------------------------------------------------------
 -- Table `farmfood`.`sellers`
 -- -------------------------------------------------------
---CREATE TABLE IF NOT EXISTS sellers (
---  id SERIAL PRIMARY KEY,
---  name TEXT,
---  description TEXT,
---  grade INTEGER,
---  user_id BIGINT
---);
+CREATE TABLE IF NOT EXISTS sellers (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  description TEXT,
+  grade INTEGER,
+  user_id BIGINT
+);
 
 -- -------------------------------------------------------
 -- Table `farmfood`.`seller_categories`
@@ -139,8 +139,11 @@ CREATE TABLE IF NOT EXISTS seller_cities (
 -- -------------------------------------------------------
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
+  login TEXT,
   password TEXT,
   userName TEXT,
+  firstname TEXT,
+  lastname TEXT,
   email TEXT UNIQUE NOT NULL,
   image TEXT,
   image_blob BYTEA
@@ -488,13 +491,19 @@ INSERT INTO seller_products (seller_id, product_id) VALUES
 -- 2. Milk and dairy products
  ((SELECT id FROM sellerId), (SELECT id FROM products WHERE name = 'Cow milk')),
 -- 3. Cheese
- ((SELECT id FROM sellerId), (SELECT id FROM products WHERE name = 'Circassian cheese')),
+ ((SELECT id FROM sellerId), (SELECT id FROM products WHERE name = 'Circassian cheese'));
+ 
+WITH sellerId AS (SELECT id FROM sellers WHERE name = 'Name Surname')
+INSERT INTO seller_products (seller_id, product_id) VALUES
 -- 4. Butter
  ((SELECT id FROM sellerId), (SELECT id FROM products WHERE name = 'Butter')),
 -- 5. Meat and meat products
  ((SELECT id FROM sellerId), (SELECT id FROM products WHERE name = 'Chicken meat')),
 -- 6. Vegetables
- ((SELECT id FROM sellerId), (SELECT id FROM products WHERE name = 'Grapes')),
+ ((SELECT id FROM sellerId), (SELECT id FROM products WHERE name = 'Grapes'));
+ 
+WITH sellerId AS (SELECT id FROM sellers WHERE name = 'Viktar Astapkovich')
+INSERT INTO seller_products (seller_id, product_id) VALUES
 -- 7. Fruits and berries
  ((SELECT id FROM sellerId), (SELECT id FROM products WHERE name = 'Apple')),
 -- 8. Honey
--- src/main/java/com/gpec/FarmFood/configs/MvcConfig.java
+++ src/main/java/com/gpec/FarmFood/configs/MvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -15,9 +16,19 @@ public class MvcConfig implements WebMvcConfigurer {
     @Bean
     public ViewResolver getViewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/public/");
+//        resolver.setPrefix("/public/");
         resolver.setSuffix(".html");
         return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/frontend/public/**")
+                .addResourceLocations("/frontend/public/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("/static/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 
 //    @Override
